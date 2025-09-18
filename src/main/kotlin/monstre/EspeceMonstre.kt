2464f3ae09e456fe537ccad530613289f109db1f
Kellyane DIAKUMPUNA <yauth11@gmail.com>
2025-09-18 12:08:31 +0200

    debut de la classe especemonstre

---
+++ src/main/kotlin/monstre/EspeceMonstre.kt
@@ -0,0 +1,35 @@
+package org.example.monstre
+
+/**
+ * Cette classe repésente les différentes espèces de monstres
+ *
+ * definir blahblah
+ * blahblah
+ *
+ * @property id L'identifiant unique de l'entraîneur.
+ * @property nom Le nom de l'entraîneur.
+ * @property type La quantité d'argent en possession de l'entraîneur.
+ * @property baseAttaque
+ * @property baseDefense
+ * @property baseVitesse
+ * @property baseAttaqueSpe
+ * @property baseDefenseSpe
+ * @property basePv
+ * @property modAttaque
+ * @property modDefense
+ * @property modVitesse
+ * @proprety modAttaqueSpe
+ * @property modDefenseSpe
+ * @property modPv
+ * @property description
+ * @property particularites
+ * @property caractères
+
+ */
+
+class EspeceMonstre {
+
+
+
+
+}
