f164f66465487d6af9d1425182f56e2ccc8188c7
Kellyane L. Diakumpuna <yauth11@gmail.com>
2025-09-21 01:35:12 +0200

    commencement classe individuMonstre

--- src/main/kotlin/monstre/IndividuMonstre.kt
+++ src/main/kotlin/monstre/IndividuMonstre.kt
@@ -1,12 +1,40 @@
 package org.example.monstre
 
+import org.example.dresseur.Entraineur
+
 /**
- * Cette classe représente
+ * Cette classe représente chaque individu, c'est-à-dire les monstres avec le joueur va interagir
+ * (les monstres sauvages, les monstres de l’équipe du joueur, les monstres des autres dresseurs).
+ *
+ * Il y a un nombre conséquent de propriétés et de méthodes.
  *
  *
- *
+ * @constructor IndividuMonstre :
+ * @param expInit
+ * @property id
+ * @property nom
+ * @property espece
+ * @property entraineur
+ * @property niveau
+ * @property attaque
+ * @property
+ * @property
+ * @property
+ * @property
  */
 
-class IndividuMonstre {
+class IndividuMonstre constructor(
+    var id: Int,
+    var nom: String,
+    var espece: EspeceMonstre,
+    var entraineur: Entraineur? = null,
+    expInit: Double
+) {
+    var niveau: Int = 1
+    //var attaque: Int =
+
+
+    //var potentiel = (0.5..2.0)
+
 
 }
