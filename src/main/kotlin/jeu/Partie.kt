cb2e6860da2f4b4af86b26f69b95d7cf74b7457e
Kellyane L. Diakumpuna <yauth11@gmail.com>
2025-10-01 23:12:44 +0200

    Commencement de la classe Partie

--- src/main/kotlin/jeu/Partie.kt
+++ src/main/kotlin/jeu/Partie.kt
@@ -1,6 +1,43 @@
 package org.example.jeu
 
-class Partie (
+import org.example.dresseur.Entraineur
+import org.example.especeAquamy
+import org.example.especeFlamkip
+import org.example.especeSpringleaf
+import org.example.monde.Zone
+import org.example.monstre.IndividuMonstre
 
+/**
+ * Cette classe ..
+ *
+ * @property id
+ * @property joueur
+ * @property zone
+ */
+
+class Partie (
+    var id : Int,
+    var joueur : Entraineur,
+    var zone : Zone
 ) {
+
+    /**
+     * Cette méthode
+     *
+     *
+     */
+
+    fun choixStarter() {
+        val monstre1 = IndividuMonstre(1,"Springleaf", especeSpringleaf,null,1500.0)
+        val monstre2 = IndividuMonstre(2,"Flampkip", especeFlamkip,null,1500.0)
+        val monstre3 = IndividuMonstre(3,"Aquamy", especeAquamy,null,1500.0)
+
+        monstre1.afficheDetail() //Springleaf
+        monstre2.afficheDetail() //Flampkip
+        monstre3.afficheDetail() //Aquamy
+
+        println()
+    }
+
+
 }
